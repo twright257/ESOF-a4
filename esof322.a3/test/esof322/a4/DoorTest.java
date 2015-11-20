0e0cbf6b35289f4f33f8627811370d19e4e7d65d
Tyler Wright <twright257@gmail.com>
2015-11-19 21:12:39 -0700

    mas

---
+++ esof322.a3/test/esof322/a4/DoorTest.java
@@ -0,0 +1,65 @@
+package esof322.a4;
+
+/**
+ * Tyler Wright
+ * Oct. 26, 2015
+ * DoorTest method tests methods in Door class
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import esof322.a4.Door;
+import esof322.a4.Key;
+import esof322.a4.Player;
+import esof322.a4.Room;
+import esof322.a4.Wall;
+
+public class DoorTest {
+    Room r1; 
+    Room r2; 
+    Wall w1; 
+    Key k1; 
+    Player p; 
+
+	@Before
+	public void setUp() throws Exception {
+	    r1 = new Room(); 
+	    r2 = new Room(); 
+	    w1 = new Wall(); 
+	    k1 = new Key(); 
+	    p = new Player(); 
+	}
+
+    /**
+     * Test of enter method, of class Door.
+     */
+    @Test
+    public void testEnter() {
+        System.out.println("enter");
+        r1.addItem(k1);
+        p.setLoc(r1);
+        //player without key r1 to r2
+        Door instance = new Door(r1, r2, k1);
+        instance.enter(p);
+        assertEquals("did not go through door", r1, p.getLoc()); 
+        //player with key r1 to r2
+        p.pickUp(k1);
+        instance.enter(p);
+        assertEquals("go through door", r2, p.getLoc());
+        
+        r2.addItem(k1);
+        p.drop(1);
+        p.setLoc(r2);
+        //player without key r2 to r1
+        instance.enter(p);
+        assertEquals("did not go through door", r2, p.getLoc());
+        //player with key r2 to r1
+        p.pickUp(k1);
+        instance.enter(p);
+        assertEquals("go through door", r1, p.getLoc()); 
+    }
+
+}
---
+++ esof322.a3/test/esof322/a4/PlayerTest.java
@@ -0,0 +1,134 @@
+package esof322.a4;
+
+/**
+ * Tyler Wright
+ * Oct. 26, 2015
+ * PlayerTest class tests methods in player class 
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import esof322.a4.Item;
+import esof322.a4.Player;
+import esof322.a4.Room;
+
+public class PlayerTest {
+	Player p; 
+	Item item; 
+	Room r1; 
+	
+	@Before
+	public void setUp() throws Exception {
+		p = new Player(); 
+		item = new Item(); 
+		item.setDesc("key");
+		r1 = new Room(); 
+	}
+	
+    /**
+     * Test of getLocation method, of class Player.
+     */
+    @Test
+    public void testGetLocation() {
+        System.out.println("get location");
+        //location null
+        assertNull(p.getLoc()); 
+    }
+	
+    /**
+     * Test of setLocation method, of class Player.
+     */
+    @Test
+    public void testSetLocation() {
+        System.out.println("set location");
+        //location null
+        assertNull(p.getLoc()); 
+        p.setLoc(r1);
+        //location equal to r1
+        assertEquals("location = r1", r1, p.getLoc()); 
+    }
+
+    /**
+     * Test of go method, of class Player.
+     */
+    @Test
+    public void testGo() {
+        System.out.println("go");
+        int direction = 0;
+        p.setLoc(r1);
+        Room r2 = new Room(); 
+        r1.setSide(0, r2);
+        p.go(direction);
+        //test that player now in location corresponding to direction 
+        assertEquals("new location r2", r2, p.getLoc()); 
+        //test go in wall direction 
+        p.go(1);
+        assertEquals("player did not move from r2", r2, p.getLoc()); 
+    }
+
+    /**
+     * Test of showMyThings method, of class Player.
+     */
+    @Test
+    public void testShowMyThings() {
+        System.out.println("show my things");
+        assertEquals("no items", "", p.showMyThings()); 
+    }
+    
+    /**
+     * Test of pickUp method, of class Player.
+     */
+    @Test
+    public void testPickUp() {
+        System.out.println("pickUp");
+        r1.addItem(item);
+        p.setLoc(r1);
+        assertEquals("no items", "", p.showMyThings()); 
+        //1 item picked up
+        p.pickUp(item);
+        assertEquals("key in items", "1: key ", p.showMyThings()); 
+        //2 items picked up
+        p.pickUp(item);
+        assertEquals("two keys in items", "1: key 2: key ", p.showMyThings()); 
+        //3rd pickup attempt
+        p.pickUp(item);
+        assertEquals("two keys in items", "1: key 2: key ", p.showMyThings()); 
+    }
+
+
+
+    /**
+     * Test of drop method, of class Player.
+     */
+    @Test
+    public void testDrop() {
+        System.out.println("drop");
+        p.setLoc(r1);
+        r1.addItem(item);
+        //drop with no items
+        p.drop(0);
+        p.drop(1);
+        assertEquals("no items", "", p.showMyThings()); 
+        //drop with 1 item 
+        p.pickUp(item);
+        assertEquals("key in items", "1: key ", p.showMyThings()); 
+        p.drop(2);
+        assertEquals("key in items", "1: key ", p.showMyThings()); 
+        p.drop(1);
+        assertEquals("key not in items", "", p.showMyThings()); 
+        //drop with 2 items
+        p.pickUp(item);
+        p.pickUp(item);
+        assertEquals("2 key in items", "1: key 2: key ", p.showMyThings()); 
+        p.drop(2);
+        assertEquals("key in items", "1: key ", p.showMyThings()); 
+        p.drop(1);
+        assertEquals("key not in items", "", p.showMyThings()); 
+    }
+    
+
+
+}
