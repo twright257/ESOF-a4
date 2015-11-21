5cfe8c32a75bf6c578fc95b177349ad08a42a192
Tyler Wright <twright257@gmail.com>
2015-11-20 17:48:16 -0700

    welcomewindow

--- esof322.a4/src/esof322/a4/AdventureGameView.java
+++ esof322.a4/src/esof322/a4/AdventureGameView.java
@@ -1,126 +1,124 @@
 package esof322.a4;
 
-
 import BreezySwing.*;
 
 import javax.swing.*;
 
-public class AdventureGameView extends GBFrame{
+public class AdventureGameView extends GBFrame {
 
-   /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-// Window objects --------------------------------------
-   JLabel welcomeLabel =
-     addLabel("Welcome to the Adventure Game " + 
-              "(inspired by an old game called the Colossal Cave Adventure)." +
-            " Java implementation Copyright (c) 1999-2012 by James M. Bieman",
-	    1,1,5,1);
-   
-   JLabel viewLable = addLabel ("Your View: ",2,1,1,1);
-   JTextArea viewArea = addTextArea("Start",3,1,4,3); 
+	
+	
+	// Window objects --------------------------------------
+	JLabel welcomeLabel = addLabel(
+			"Welcome to the Adventure Game " + "(inspired by an old game called the Colossal Cave Adventure)."
+					+ " Java implementation Copyright (c) 1999-2012 by James M. Bieman",
+			1, 1, 5, 1);
 
-   JLabel carryingLable = addLabel ("You are carying: ",6,1,1,1);
-   JTextArea carryingArea = addTextArea("Nothing",7,1,4,3); 
-   
-   JLabel activityLabel = addLabel("Interaction Info", 2, 5, 1, 1); //third text box label
-   JTextArea activityArea = addTextArea("", 3, 4, 2, 2);    //third text box
+	JLabel viewLable = addLabel("Your View: ", 2, 1, 1, 1);
+	JTextArea viewArea = addTextArea("Start", 3, 1, 4, 3);
 
-JLabel separator1 = addLabel
-   ("-----------------------------------------------------------------"
-	 , 10,1,4,1);
+	JLabel carryingLable = addLabel("You are carying: ", 6, 1, 1, 1);
+	JTextArea carryingArea = addTextArea("Nothing", 7, 1, 4, 3);
 
+	JLabel activityLabel = addLabel("Interaction Info", 2, 5, 1, 1); // third text box label
 
-   JLabel choiceLabel    = addLabel
-      ("Choose a direction, pick-up, or drop an item" ,11,1,5,1);
+	JTextArea activityArea = addTextArea("", 3, 4, 2, 2); // third text box
 
-   JButton grabButton1 = addButton ("Grab Item 1", 11, 5, 1, 1); //choose to grab first item 
-   JButton grabButton2 = addButton ("Grab Item 2", 12, 5,1,1);  //choose to grap second item 
-   JButton dropButton1 = addButton ("Drop Item 1", 13, 5, 1, 1); //choose to grab first item 
-   JButton dropButton2 = addButton ("Drop Item 2", 14, 5,1,1);
-   
-   JButton northButton = addButton ("North", 12,2,1,1);
-   JButton southButton = addButton ("South", 14,2,1,1);
-   JButton eastButton = addButton ("East",   13,3,1,1);
-   JButton westButton = addButton ("West",   13,1,1,1);
-   JButton upButton = addButton ("Up", 12,3,1,1);
-   JButton downButton = addButton ("Down", 14,3,1,1);
+	JLabel separator1 = addLabel("-----------------------------------------------------------------", 10, 1, 4, 1);
 
-   AdventureGameModelFacade model;
-   
-   // Constructor-----------------------------------------------
+	JLabel choiceLabel = addLabel("Choose a direction, pick-up, or drop an item", 11, 1, 5, 1);
 
-   public AdventureGameView(){
-      setTitle ("Adventure Game");
-      model = new AdventureGameModelFacade();
+	JButton saveButton = addButton("Save Game", 8, 5, 1, 1); // button for
+																// saving
+	JButton loadButton = addButton("Load Game", 9, 5, 1, 1); // button for
+																// loading
 
-      viewArea.setEditable (false);
-      carryingArea.setEditable (false);
-      displayCurrentInfo();
-   } 
-   
-   
-   // buttonClicked method--------------------------------------
+	JButton grabButton1 = addButton("Grab Item 1", 11, 5, 1, 1); // choose to grab first item 
 
-   public void buttonClicked (JButton buttonObj){
-      if (buttonObj == upButton)
-         model.goUp();
+	JButton grabButton2 = addButton("Grab Item 2", 12, 5, 1, 1); // choose to grab second item
 
-      else if (buttonObj == downButton)
-	 model.goDown();
+	JButton dropButton1 = addButton("Drop Item 1", 13, 5, 1, 1); // choose to drop first item
 
-      else if (buttonObj == northButton)
-	 model.goNorth();
+	JButton dropButton2 = addButton("Drop Item 2", 14, 5, 1, 1);
 
-      else if (buttonObj == southButton)
-	 model.goSouth();
+	JButton northButton = addButton("North", 12, 2, 1, 1);
+	JButton southButton = addButton("South", 14, 2, 1, 1);
+	JButton eastButton = addButton("East", 13, 3, 1, 1);
+	JButton westButton = addButton("West", 13, 1, 1, 1);
+	JButton upButton = addButton("Up", 12, 3, 1, 1);
+	JButton downButton = addButton("Down", 14, 3, 1, 1);
 
-      else if (buttonObj == eastButton)
-	 model.goEast();
+	AdventureGameModelFacade model;
 
-      else if (buttonObj == westButton)
-	 model.goWest();
+	// Constructor-----------------------------------------------
 
-      else if (buttonObj == grabButton1)
-         grab(1); 
-      
-      else if (buttonObj == grabButton2)
-	 grab(2);
+	public AdventureGameView() {
+		setTitle("Adventure Game");
+		model = new AdventureGameModelFacade();
 
-      else if (buttonObj == dropButton1)
-	 drop(1);
-      
-      else if (buttonObj == dropButton2)
-          drop(2);
+		viewArea.setEditable(false);
+		carryingArea.setEditable(false);
+		displayCurrentInfo();
+	}
 
-      displayCurrentInfo();
-  }
-      
-   
-   // Private methods-------------------------------------------
+	// buttonClicked method--------------------------------------
 
-   //print to text areas
-   private void displayCurrentInfo(){
-	 viewArea.setText(model.getView());
-	 carryingArea.setText(model.getItems());
-         activityArea.setText(model.getWallFlag()); 
-	 }
+	public void buttonClicked(JButton buttonObj) {
+		if (buttonObj == upButton)
+			model.goUp();
 
-    // call to pickup item 
-   private void grab(int item) {
-       model.pickUpItem(item);
-   }
+		else if (buttonObj == downButton)
+			model.goDown();
 
-    // Left as an exercise. 
-    private void drop(int item) {
-        model.dropItem(item);
-   }
+		else if (buttonObj == northButton)
+			model.goNorth();
 
-   public static void main (String[] args){
-      JFrame view = new AdventureGameView();
-      view.setSize (800, 600); /* was 400, 250  */             
-      view.setVisible(true);    
-   }                    
+		else if (buttonObj == southButton)
+			model.goSouth();
+
+		else if (buttonObj == eastButton)
+			model.goEast();
+
+		else if (buttonObj == westButton)
+			model.goWest();
+
+		else if (buttonObj == grabButton1)
+			grab(1);
+
+		else if (buttonObj == grabButton2)
+			grab(2);
+
+		else if (buttonObj == dropButton1)
+			drop(1);
+
+		else if (buttonObj == dropButton2)
+			drop(2);
+
+		displayCurrentInfo();
+	}
+
+	// Private methods-------------------------------------------
+
+	// print to text areas
+	private void displayCurrentInfo() {
+		viewArea.setText(model.getView());
+		carryingArea.setText(model.getItems());
+		activityArea.setText(model.getWallFlag());
+	}
+
+	// call to pickup item
+	private void grab(int item) {
+		model.pickUpItem(item);
+	}
+
+	// Left as an exercise.
+	private void drop(int item) {
+		model.dropItem(item);
+	}
+
 }
---
+++ esof322.a4/src/esof322/a4/WelcomeView.java
@@ -0,0 +1,43 @@
+package esof322.a4;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import BreezySwing.*;
+
+public class WelcomeView extends GBFrame{
+	JLabel welcomeLabel = addLabel(	"                           New Game Difficulty", 1, 5, 1, 1);
+	JButton easyButton = addButton("Easy", 11, 5, 1, 1); // choose easy difficulty
+	JButton hardButton = addButton("Hard", 15, 5, 1, 1); // choose to grab first item 
+	JButton loadButton = addButton("Load Game", 21, 5, 1, 1); // choose to grab first item 
+	
+	JLabel separator1 = addLabel("----------------------------------------------------------------------", 20, 5, 1, 1);
+
+	
+	public WelcomeView() {
+		setTitle("Adventure Game"); 
+	}
+	
+	public void buttonClicked(JButton buttonObj) {
+		if (buttonObj == easyButton) {
+			JFrame view = new AdventureGameView();
+			view.setSize(800, 600); /* was 400, 250 */
+			view.setVisible(true);
+			this.dispose();
+		}else if (buttonObj == hardButton) {
+			
+		}else if (buttonObj == loadButton) {
+			
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		JFrame welcome = new WelcomeView(); 
+		welcome.setSize(300, 200); 
+		welcome.setVisible(true);
+		welcome.setResizable(false); 
+	}
+}
+
